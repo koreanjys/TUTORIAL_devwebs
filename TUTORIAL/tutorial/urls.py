393a904db51989e4f3ff1b185be710d72b4d8a1d
koreanjys <connecttojys@gmail.com>
2023-11-15 10:19:42 +0900

    question_crud

--- TUTORIAL/tutorial/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('blog/', include('blog.urls')),
-    path('admin/', admin.site.urls),
-    path('', include('single_pages.urls')),
-]
